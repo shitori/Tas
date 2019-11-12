6f88880f2d6bd1e9fd1ffaba61d28bb7506f9099
Antony Fourchault <antonyfourchault@gmail.com>
2019-11-12 21:36:20 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*BinaryHeap<Integer> a = new BinaryHeap<Integer>();
+        a.insert(3);
+        a.insert(4);
+        a.insert(45);
+        a.insert(49);
+        a.insert(10);
+        a.insert(78);
+        a.insert(56);
+        a.insert(61);
+        a.insert(54);
+        a.insert(72);
+        a.extractMin();
+        a.extractMin();
+        a.extractMin();
+        a.extractMin();
+        a.printer();*/
+
+    }
+}
